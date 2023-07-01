2e75d1251d51d53893b6fa80d2d4c0e37d40419b
Александр <alexandrvip@icloud.com>
2023-07-01 16:25:20 +0300

    init

---
+++ lesson_4_1.py
@@ -0,0 +1,13 @@
+def strcounter(s: str):
+    symbols_dict = {}
+    for symbol in s:
+        if symbol in symbols_dict:
+            symbols_dict[symbol] += 1
+        else:
+            symbols_dict[symbol] = 1
+    print(symbols_dict)
+
+
+
+strcounter('aaaaaaaaaaaaaaaahhhhhhhhhhhhhfkdfjdlkfmsdnl;sdvnadjkdnvdnfjkvndjkanv;jkn')
+
